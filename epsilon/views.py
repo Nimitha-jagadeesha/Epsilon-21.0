a6f1aea1cfe0d44592a0f15d92200a247d8fda9a
Nimitha jagadeesha <nimitha1jagadeesha@gmail.com>
2021-05-28 18:45:53 +0530

    commit

--- epsilon/views.py
+++ epsilon/views.py
@@ -56,10 +56,10 @@ def register(request):
 @login_required
 def arena(request):
     if(request.user.score.question_number==0):
-        number = random.randint(1,Question.objects.size())
+        number = random.randint(1,Question.objects.count())
         Score.objects.filter(user=request.user).update(
                 question_number=number)
-        # Score.objects.filter(user=request.user)[0].picked.append(number)
+        Score.objects.filter(user=request.user)[0].picked.append(number)
     question = Question.objects.filter(
         number=request.user.score.question_number).first()
     x = False
