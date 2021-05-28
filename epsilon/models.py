da135d9dd18944e0a67702d79de91ce9f7ddc3e6
Nimitha jagadeesha <nimitha1jagadeesha@gmail.com>
2021-05-28 14:12:50 +0530

    init

---
+++ epsilon/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from PIL import Image
+# Create your models here.
+class Question(models.Model):
+    number = models.IntegerField(default=0)
+    question = models.TextField()
+    answer = models.TextField()
+    date_created = models.DateTimeField(default = timezone.now)
+    image = models.TextField(default='none.jpg')
+
+    def __str__(self):
+        return str(self.number)
+
+class Score(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    points = models.IntegerField(default=0)
+    question_number = models.IntegerField(default=1)
+    last_submit = models.DateTimeField(default=timezone.now())
+    visible = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'{self.user.username} ({self.last_submit})'
---
+++ epsilon/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm
+from .models import Score, Question
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth.views import LogoutView
+
+class YourCustomLogoutView(LogoutView):
+
+    def get_next_page(self):
+        next_page = super(YourCustomLogoutView, self).get_next_page()
+        messages.add_message(
+            self.request, messages.SUCCESS,
+            'You successfully log out!'
+        )
+        return next_page
+
+def login_excluded(redirect_to):
+    """ This decorator kicks authenticated users out of a view """ 
+    def _method_wrapper(view_method):
+        def _arguments_wrapper(request, *args, **kwargs):
+            if request.user.is_authenticated:
+                return redirect('Home') 
+            return view_method(request, *args, **kwargs)
+        return _arguments_wrapper
+    return _method_wrapper
+
+def home(request):
+    return render(request, 'epsilon/home.html')
+
+
+def learderboard(request):
+    list = ['-points', 'last_submit']
+    Scoreboard = Score.objects.filter(visible = True).order_by(*list)
+    return render(request, 'epsilon/leaderboards.html', {'Scoreboard': Scoreboard})
+
+@login_excluded('app:redirect_to_view')
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(
+                request, f'Your account has been created ! You can now login')
+            return redirect(reverse('login'))
+    else:
+        form = UserRegisterForm()
+    return render(request, 'epsilon/register.html', {'form': form})
+
+
+@csrf_exempt
+@login_required
+def arena(request):
+    question = Question.objects.filter(
+        number=request.user.score.question_number).first()
+    x = False
+    try:
+        if question.image != 'none.jpg':
+            x = True
+    except:
+        pass
+    if request.method == 'POST':
+        answer = request.POST.get('answer')
+        answer = answer.replace(" ","")
+        answer = answer.replace(".","")
+        if  not Question.objects.filter(number=request.user.score.question_number).first():
+            return redirect('Arena')
+        crct_answer = Question.objects.filter(
+            number=request.user.score.question_number).first().answer
+        crct_answer =crct_answer.replace(" ","")
+        crct_answer = crct_answer.replace(".","")
+        if answer.lower().strip() == crct_answer.lower():
+            Score.objects.filter(user=request.user).update(
+                question_number=request.user.score.question_number+1)
+            Score.objects.filter(user=request.user).update(
+                points=request.user.score.points+1)
+            Score.objects.filter(user=request.user).update(
+                last_submit=timezone.now())
+            question = Question.objects.filter(
+                number=request.user.score.question_number).first()
+            return redirect('Arena')
+            try:
+                if question.image != 'none.jpg':
+                    x = True
+            except:
+                x = False
+        else:
+            messages.warning(request, 'Wrong answer :( Try again!')
+            try:
+                if question.image != 'none.jpg':
+                    x = True
+            except:
+                x = False
+            return render(request, 'epsilon/arena.html', {'question': question,'x':x})
+    if question == None:
+         return render(request, 'epsilon/arena.html', {'done': True})
+    return render(request, 'epsilon/arena.html', {'question': question,'x':x})
